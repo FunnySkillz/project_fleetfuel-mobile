dd27db7a0394d43380086875b7358b70b87bdefe
FunnySkillz <sebastijan.bogdan@outlook.com>
2026-04-03 11:47:11 +0200

    repo: export change history repository

--- src/data/repositories/index.ts
+++ src/data/repositories/index.ts
@@ -1,3 +1,4 @@
+export { changeHistoryRepo } from './change-history-repo';
 export { entriesRepo } from './entries-repo';
 export { fuelRepo } from './fuel-repo';
 export { logsRepo } from './logs-repo';
