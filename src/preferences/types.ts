359302d0a13d0966e6fae9978abd638976f39144
FunnySkillz <sebastijan.bogdan@outlook.com>
2026-04-06 23:21:31 +0200

    feat(shared-fleet): update src/preferences/types.ts

--- src/preferences/types.ts
+++ src/preferences/types.ts
@@ -1,8 +1,10 @@
 export type ThemeMode = 'system' | 'light' | 'dark';
 export type AppLanguage = 'en' | 'de';
+export type AppMode = 'local' | 'shared';
 
 export type AppPreferences = {
   themeMode: ThemeMode;
   language: AppLanguage;
   appLockEnabled: boolean;
+  appMode: AppMode;
 };
