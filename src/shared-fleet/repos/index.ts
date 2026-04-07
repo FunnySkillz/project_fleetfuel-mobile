20cb303b89ae1e856b5ea9d7a9e72e6d28d5337b
FunnySkillz <sebastijan.bogdan@outlook.com>
2026-04-07 06:10:56 +0200

    feat(shared-fleet): update src/shared-fleet/repos/index.ts

--- src/shared-fleet/repos/index.ts
+++ src/shared-fleet/repos/index.ts
@@ -1,6 +1,14 @@
-export type { AuthRepo, FleetRepo, InvitesRepo, VehicleAccessRepo, AuthCredentials } from './contracts';
+export type {
+  AssignmentRepo,
+  AuthRepo,
+  FleetRepo,
+  InvitesRepo,
+  VehicleAccessRepo,
+  AuthCredentials,
+} from './contracts';
 
 export { authRepo } from './auth-repo';
+export { assignmentRepo } from './assignment-repo';
 export { fleetRepo } from './fleet-repo';
 export { invitesRepo } from './invites-repo';
 export { vehicleAccessRepo } from './vehicle-access-repo';
